1b417c9115cc3f896340d8ff8eec5ef767eadf55
Peter Okerulu <oduchep@gmail.com>
2024-07-06 23:36:26 +0100

    configure mongodb

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.use(errorHandler);
 const port = process.env.PORT || 6005;
 
 // Connect to database
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log(`Connected to MongoDB`);
     app.listen(port, () => {
