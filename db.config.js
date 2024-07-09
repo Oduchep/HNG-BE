103fe2569477310a7f7f19a21b3a97d54e8c01d0
Peter Okerulu <oduchep@gmail.com>
2024-07-09 01:16:40 +0100

    added pg package

--- db.config.js
+++ db.config.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   protocol: 'postgres',
   logging: false, // Set to console.log to see the raw SQL queries
   dialectOptions: {
